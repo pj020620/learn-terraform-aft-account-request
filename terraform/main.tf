ee038911bf669ba67784d79b8a6ec070fc494f99
pj020620 <117139342+pj020620@users.noreply.github.com>
2023-01-09 17:58:56 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "prabu_jaganathan@nsearchglobal.com"
-    AccountName               = "prabu-nsearch"
+    AccountEmail              = ""
+    AccountName               = ""
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "prabu_jaganathan@nsearchglobal.com"
-    SSOUserFirstName          = "Prabu"
-    SSOUserLastName           = "Jagan"
+    SSOUserEmail              = ""
+    SSOUserFirstName          = ""
+    SSOUserLastName           = ""
   }
 
   account_tags = {
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "Prabu_nsearch"
+  account_customizations_name = ""
 }
